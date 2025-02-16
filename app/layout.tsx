10e353ae39962533d00217d2cffd22596f3973f3
김윤진 <sion11290805@gmail.com>
2025-02-16 17:03:54 +0900

    feat: html에 manifest json 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ko">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
